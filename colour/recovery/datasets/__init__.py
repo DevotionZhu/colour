9eb4b6d9ef179cdd7f1df93353589294fdf5112e
Paweł Redman <pawel.redman@gmail.com>
2020-08-07 18:37:34 +0200

    Implement Mallett and Yuksel (2019) reflectance recovery method.

--- colour/recovery/datasets/__init__.py
+++ colour/recovery/datasets/__init__.py
@@ -2,6 +2,8 @@
 
 from __future__ import absolute_import
 
+from .mallett2019 import MALLETT_2019_SRGB_SHAPE, MALLETT_2019_SRGB_BASIS
 from .smits1999 import SMITS_1999_SDS
 
-__all__ = ['SMITS_1999_SDS']
+__all__ = ['MALLETT_2019_SRGB_SHAPE', 'MALLETT_2019_SRGB_BASIS']
+__all__ += ['SMITS_1999_SDS']
